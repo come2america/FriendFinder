3350fac203866af032684c2645767521c017e2ad
Yusuf <ysserunjogi@gmail.com>
2019-02-16 14:04:06 -0800

    fix please

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require("path");
-var mysql = require('mysql');
+
  var app = express();
  var PORT = process.env.PORT || 3000;
 
@@ -10,36 +10,11 @@ app.use(express.json());
 require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'friendfinder'
-  });
-  
-  connection.connect();
-  
-  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The connection is: good ' + connection.threadId);
-   
-    
-  });
-
-
-
  
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
-
-       
-
-    
 //Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
